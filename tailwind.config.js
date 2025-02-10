662f002b6ab7ae2b821702e81656f06ab42abe1c
Youssef Saroute <youssef.saroute77@gmail.com>
2025-02-11 00:10:04 +0100

    tailwind done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
+const colors = require('tailwindcss/colors')
+
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.{erb,haml,html,slim}',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: '#006D77',
+      accent: '#83C5BE',
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        heading: ['Clash Display', 'Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+        body: ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
 }
