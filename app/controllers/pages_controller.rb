869c85388b1bd25ae9b1ded39606fb7a631e7c50
Youssef Saroute <youssef.saroute77@gmail.com>
2025-02-10 17:36:23 +0100

    Initial commit: Portfolio website setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def projects
+  end
+
+  def skills
+  end
+
+  def testimonials
+  end
+
+  def contact
+  end
+end
